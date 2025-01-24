2341cd6cbc787b0c1e7d2aa92e4c58e680b91b45
N-serena <nobserena@gmail.com>
2025-01-24 13:03:33 -0500

    header done

--- app/main.py
+++ app/main.py
@@ -5,15 +5,16 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
+    # UDP socket is created and bound to the local address 127.0.0.1 on port 2053
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
     
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
+
             response = b""
+            response = b"\x04\xd2\x80" + (b"\x00" * 9)
     
             udp_socket.sendto(response, source)
         except Exception as e:
